0caea454ab186cdd26330f2648a8acd28a58607a
Dell-User <Dell-User@DESKTOP-GAPN8T9>
2023-10-10 11:10:30 +0200

    Add project files.

---
+++ 24.Abstract/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace _24.Abstract
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Vehicle car1 = new vehicle():
+            corsa car2 = new corsa();
+            Console.WriteLine(car2.wheels);
+
+            Console.ReadLine();
+        }
+    }
+}
+abstract class vehicle
+{
+    public int wheels = 4;
+}
+class corsa: vehicle
+{
+    public int sparewheels = 1;
+}
